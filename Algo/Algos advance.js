89b9bc4ec169ee3fc8cb9715b1fe3177f1acfbbf
Thierry Steenhoudt <amaroknt@outlook.fr>
2024-09-23 13:14:19 +0200

    ajout algo advance

---
+++ Algo/Algos advance.js
@@ -0,0 +1,19 @@
+// exo 0 -----------------------------------------------------
+// function countNum(num) {
+//     for (let index = 1; index <= num ; index++) {
+//         if (index % 3 === 0 && index % 5 === 0 ) {
+//             console.log("FizzBuzz")
+//         } else if (index % 3 === 0) {
+//             console.log("Fizz")
+//         } else if (index % 5 === 0){
+//             console.log("Buzz")
+//         } else {
+//             console.log(index)
+//         }
+//     }
+// }
+
+// countNum(30)
+
+// exo 1-----------------------------------------------------
+
